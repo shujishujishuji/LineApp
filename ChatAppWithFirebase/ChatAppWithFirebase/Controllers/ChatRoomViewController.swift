0e3dd20a49d09b475c507301bdc6dbb7ad32e246
shujishujishuji <shuji00881199@gmail.com>
2021-09-09 03:58:26 +0900

    メッセージ送信画面追加

--- ChatAppWithFirebase/ChatAppWithFirebase/Controllers/ChatRoomViewController.swift
+++ ChatAppWithFirebase/ChatAppWithFirebase/Controllers/ChatRoomViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 class ChatRoomViewController: UIViewController {
     
     private let cellId = "cellId"
+    private var messages = [String]()
+    
+    private lazy var chatInputAccessoryView: ChatInputAccessoryView = {
+       let view = ChatInputAccessoryView()
+        view.frame = .init(x: 0, y: 0, width: view.frame.width, height: 100)
+        view.delegate = self
+        return view
+    }()
     
     @IBOutlet weak var chatRoomTableView: UITableView!
     
@@ -23,6 +31,26 @@ class ChatRoomViewController: UIViewController {
         chatRoomTableView.backgroundColor = .rgb(red: 118, green: 140, blue: 180)
     }
     
+    override var inputAccessoryView: UIView? {
+        get {
+            return chatInputAccessoryView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+}
+
+extension ChatRoomViewController: ChatInputAccessoryViewDelegate {
+    func tappedSendButton(text: String) {
+        messages.append(text)
+        chatInputAccessoryView.removeText()
+        chatRoomTableView.reloadData()
+    }
+    
+    
 }
 
 extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
@@ -33,11 +61,13 @@ extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = chatRoomTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = chatRoomTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatRoomTableViewCell
+//        cell.messageTextView.text = messages[indexPath.row]
+        cell.messageText = messages[indexPath.row]
         return cell
     }
     
