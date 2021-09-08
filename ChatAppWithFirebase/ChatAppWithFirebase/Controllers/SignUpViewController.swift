431b046ac9e758ab1b26f778ece529e3b21bbdb8
shujishujishuji <shuji00881199@gmail.com>
2021-09-09 07:35:01 +0900

    登録画面の追加

---
+++ ChatAppWithFirebase/ChatAppWithFirebase/Controllers/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by taniguchi shuji on 2021/09/09.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var profileImageButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var alreadyHaveAccountButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        profileImageButton.layer.cornerRadius = 85
+        profileImageButton.layer.borderWidth = 1
+        profileImageButton.layer.borderColor = UIColor.rgb(red: 240, green: 240, blue: 240).cgColor
+        
+        registerButton.layer.cornerRadius = 12
+    }
+}
