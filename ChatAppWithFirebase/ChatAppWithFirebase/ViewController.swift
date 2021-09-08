f608a5cbaefe7668bbef07a8789a67b1e1e5ee3f
shujishujishuji <shuji00881199@gmail.com>
2021-09-09 00:08:49 +0900

    メッセージリストの作成

---
+++ ChatAppWithFirebase/ChatAppWithFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by taniguchi shuji on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
